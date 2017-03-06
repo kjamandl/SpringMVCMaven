88cdd62e04c0797b02a8a0318873d2f7e1ceffc5
kjamandl <kjamandl@gmail.com>
2017-03-06 19:29:14 +0530

    Test file checkin

---
+++ SpringMVCMaven/src/main/java/com/jk/sprtest/model/Test.java
@@ -0,0 +1,14 @@
+package com.jk.sprtest.model;
+
+public class Test {
+ private String user;
+
+public String getUser() {
+	return user;
+}
+
+public void setUser(String user) {
+	this.user = user;
+}
+ 
+}
